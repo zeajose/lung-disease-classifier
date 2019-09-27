f69161d27bceac42336e1dd5c61fda9d7610384c
Jose Zea <josedavid.zea@gmail.com>
2019-09-27 09:59:05 -0400

    updated uploaded

--- flaskSaaS-master/app/views/main.py
+++ flaskSaaS-master/app/views/main.py
@@ -1,7 +1,6 @@
 from flask import render_template, jsonify, Flask, redirect, url_for, request
 from app import app
 import random
-import keras
 import os
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
@@ -20,20 +19,20 @@ def upload_file2():
 
 @app.route('/uploaded', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+  if request.method == 'POST':
+    f = request.files['file']
+    path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
 
-      model= ResNet50(weights='imagenet')
-      img = image.load_img(path, target_size=(224,224))
-      x = image.img_to_array(img)
-      x = np.expand_dims(x, axis=0)
-      x = preprocess_input(x)
-      preds = model.predict(x)
-      preds_decoded = decode_predictions(preds, top=3)[0]
-      print(decode_predictions(preds, top=3)[0])
-      f.save(path)
-      return render_template('uploaded.html', title='Success', predictions=preds_decoded, user_image=f.filename)
+    model= ResNet50(weights='imagenet')
+    img = image.load_img(path, target_size=(224,224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    preds_decoded = decode_predictions(preds, top=3)[0]
+    print(decode_predictions(preds, top=3)[0])
+    f.save(path)
+    return render_template('uploaded.html', title='Success', predictions=preds_decoded, user_image=f.filename)
 
 @app.route('/index')
 def index():
